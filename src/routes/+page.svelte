1690a50c78b5873071d2d9e30dd2326452dc0fb0
huyixi <hi@huyixi.com>
2024-12-20 14:10:11 +0800

    feat: fetch github file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,35 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	interface Note {
-		slug: string;
-		html: string;
-	}
-
-	let notes: Note[] = [];
-
-	async function fetchNote(slug: string): Promise<Note> {
-		const res = await fetch(`/api/notes?slug=${slug}`);
+	let indexNote: string = '';
+	async function fetchNote(slug: string) {
+		const fetchPrefix = 'https://api.github.com/repos/huyixi/evergreen-files/contents';
+		const res = await fetch(`${fetchPrefix}/${slug}.md`);
 		const data = await res.json();
-		return {
-			slug: data.data.slug,
-			html: data.data.html
-		};
+
+		const base64Content = data.content;
+
+		const decodedContent = atob(base64Content);
+
+		const { marked } = await import('marked');
+		const htmlContent = marked(decodedContent);
+
+		return htmlContent;
 	}
 
 	onMount(async () => {
-		const indexNote = await fetchNote('index');
-		console.log('indexNote:', typeof indexNote.html, indexNote);
-		const note2 = await fetchNote('note2');
-		const note3 = await fetchNote('note3');
-		notes = [indexNote, note2, note3];
-		// console.log('notes:', notes);
+		indexNote = await fetchNote('index');
+		console.log(indexNote);
 	});
 </script>
 
 <div>
 	<h1>Notes</h1>
-	{#each notes as note}
-		<div>
-			{@html note.html}
-		</div>
-	{/each}
+	<div>
+		{#if indexNote}
+			{@html indexNote}
+		{:else}
+			<p>Loading...</p>
+		{/if}
+	</div>
 </div>
